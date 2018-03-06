19516013322cd19b283489b9caf365c7930aff26
PatrickUtz <30327204+PatrickUtz@users.noreply.github.com>
2018-03-05 21:56:58 -0800

    all methods done

--- homework04/ClockSolver.java
+++ homework04/ClockSolver.java
@@ -23,8 +23,10 @@ public class ClockSolver {
   /**
    *  Class field definintions go here
    */
-  private final double MAX_TIME_SLICE_IN_SECONDS  = 1800.00;
-  private final double DEFAULT_TIME_SLICE_SECONDS = 60.0;
+  private final  double MAX_TIME_SLICE_IN_SECONDS  = 1800.00;
+  private final  double DEFAULT_TIME_SLICE_SECONDS = 60.0;
+  private static double angle = -1;
+  private static double timeSlice = -1;
   private final static double EPSILON_VALUE       = 0.1;      // small value for double-precision comparisons
 
   /**
@@ -35,37 +37,7 @@ public class ClockSolver {
     super();
   }
 
-  // /**
-  // *  Method to handle all the input arguments from the command line
-  // *   this sets up the variables for the simulation
-  // */
-  // public void handleInitialArguments( String args[] ) {
-  //   // args[0] specifies the angle for which you are looking
-  //   //  your simulation will find all the angles in the 12-hour day at which those angles occur
-  //   // args[1] if present will specify a time slice value; if not present, defaults to 60 seconds
-  //   // you may want to consider using args[2] for an "angle window"
-  //
-  //   System.out.println( "\n   Hello world, from the ClockSolver program!!\n\n" ) ;
-  //   if( 0 == args.length ) {
-  //     System.out.println( "   Sorry you must enter at least one argument\n" +
-  //                            "   Usage: java ClockSolver <angle> [timeSlice]\n" +
-  //                            "   Please try again..........." );
-  //       System.exit( 1 );
-  //   }
-  //   Clock clock = new Clock(3,4);
-  // }
-
-  /**
-  *  The main program starts here
-  *  remember the constraints from the project description
-  *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
-  *  @param  args  String array of the arguments from the command line
-  *                args[0] is the angle for which we are looking
-  *                args[1] is the time slice; this is optional and defaults to 60 seconds
-  */
-  public static void main( String args[] ) {
-    double angle = -1;
-    double timeSlice = -1;
+  public static void checkIfValid( String[] args ) {
     if( args.length < 1 ) {
       System.out.println( "No valid angle or time slice inputted.\n" );
       System.exit(-2);
@@ -97,15 +69,22 @@ public class ClockSolver {
           System.exit(-1);
         }
       }
-
+  }
+  /**
+  *  The main program starts here
+  *  remember the constraints from the project description
+  *  @see  http://bjohnson.lmu.build/cmsi186web/homework04.html
+  *  @param  args  String array of the arguments from the command line
+  *                args[0] is the angle for which we are looking
+  *                args[1] is the time slice; this is optional and defaults to 60 seconds
+  */
+  public static void main( String args[] ) {
+    checkIfValid( args );
     ClockSolver cse = new ClockSolver();
     Clock clock = new Clock( angle, timeSlice );
-    double[] timeValues = new double[3];
-    // cse.handleInitialArguments( args );
+    // double[] timeValues = new double[3];
     System.out.println( "\n   Hello world, from the ClockSolver program!!\n\n" );
     while( clock.getTotalSeconds() < 43200 ) {
-      // double hourA = clock.getHourHandAngle();
-      // double minuteA = clock.getMinuteHandAngle();
       double angleNow = clock.getHandAngle();
       if( Math.abs( angleNow - angle ) < EPSILON_VALUE ) {
         System.out.println( clock.toString() );
